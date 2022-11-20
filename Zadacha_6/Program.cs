04c4a1c934e100f37d2d7c61340e11da4eaba9f0
Manya <manjak29@yandex.ru>
2022-11-20 21:44:51 +0400

    задача6

---
+++ Zadacha_6/Program.cs
@@ -0,0 +1,11 @@
+﻿int Number = new int();
+Console.Write("Введите число: ");
+Number = Convert.ToInt32(Console.ReadLine());
+if (Number % 2 == 0) 
+{
+    Console.WriteLine("Число является чётным");
+}
+else
+{
+    Console.WriteLine("Число не является чётным");
+}
