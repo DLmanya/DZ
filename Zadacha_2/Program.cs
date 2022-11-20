b59ab1fcba04d3524d8d3683f993ce80a4f6654a
Manya <manjak29@yandex.ru>
2022-11-20 21:22:20 +0400

    задача2

---
+++ Zadacha_2/Program.cs
@@ -0,0 +1,14 @@
+﻿int Number1 = new int();
+Console.Write("Введите число: ");
+Number1 = Convert.ToInt32(Console.ReadLine());
+int Number2 = new int();
+Console.Write("Введите число: ");
+Number2 = Convert.ToInt32(Console.ReadLine());
+if(Number1 < Number2)
+{
+    Console.WriteLine (Number2);
+}
+else
+{
+    Console.WriteLine (Number1);
+}    
