0e8220b424f7d82d417b2e85a0286f352dae07fb
Manya <manjak29@yandex.ru>
2022-11-20 21:29:02 +0400

    задача4

---
+++ Zadacha_4/Program.cs
@@ -0,0 +1,15 @@
+﻿int Number1 = new int();
+Console.Write("Введите певрое число: ");
+Number1 = Convert.ToInt32(Console.ReadLine());
+int Number2 = new int();
+Console.Write("Введите второе число: ");
+Number2 = Convert.ToInt32(Console.ReadLine());
+int Number3 = new int();
+Console.Write("Введите третье число: ");
+Number3 = Convert.ToInt32(Console.ReadLine());
+int max = Number1;
+if (Number1 > max) max = Number1;
+if (Number2 > max) max = Number2;
+if (Number3 > max) max = Number3;
+Console.Write("Максимальное число");
+Console.WriteLine(max);
